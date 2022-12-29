d37fdd0af5ee2a999d6de6e882b6c0e9c3fb5777
Andre Bozzato <abozzato@gmail.com>
2022-12-29 15:09:07 -0300

    classificar variacoes pelo campo valor

--- src/services/mercadolivre.js
+++ src/services/mercadolivre.js
@@ -1,31 +1,28 @@
 export async function buscarAnuncio(mlb) {
   try {
-    const response = await fetch(`https://api.mercadolibre.com/items/MLB${mlb}`)
+    const idMlb = mlb.replace(/\D/g, '')
+    const response = await fetch(
+      `https://api.mercadolibre.com/items/MLB${idMlb}`
+    )
 
     const data = await response.json()
 
     if (response.status !== 200) throw new Error('Anuncio não encontrado')
 
-    const mapVarName = (variation) => variation.map((v) => v.name)
-
-    function groupBy(array, key) {
-      return array.reduce(
-        (acc, item) => ({
-          ...acc,
-          [item[key]]: [...(acc[item[key]] ?? []), item],
-        }),
-        {}
-      )
-    }
-
     const produto = {
       id: data.id,
       nome: data.title,
-      variacoes: data.variations.map((item) => ({
-        id: item.id,
-        nome: item.attribute_combinations.map((i) => i.name).join('/'),
-        valor: item.attribute_combinations.map((i) => i.value_name).join('/'),
-      })),
+      variacoes: data.variations
+        .map((item) => ({
+          id: item.id,
+          nome: item.attribute_combinations.map((i) => i.name).join('/'),
+          valor: item.attribute_combinations.map((i) => i.value_name).join('/'),
+        }))
+        .sort((x, y) => {
+          let a = x.valor.toUpperCase(),
+            b = y.valor.toUpperCase()
+          return a == b ? 0 : a > b ? 1 : -1
+        }),
     }
 
     return produto
